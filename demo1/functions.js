b05d8ec646b7e5cc5aa463b7d7f84d047c5cfb5e
GaneshWagh1998 <63993358+GaneshWagh1998@users.noreply.github.com>
2023-10-29 16:52:31 +0000

    scopes

---
+++ demo1/functions.js
@@ -0,0 +1,31 @@
+// function sayname(){
+//     console.log("ganesh");
+// }
+
+// sayname;
+
+
+// function add(num1 , num2) {// parameter
+
+//     console.log(num1 + num2);
+    
+// }
+
+
+// function add(num1 , num2) {// parameter
+
+//    let result = num1 + num2;
+//    return result;
+    
+// }
+
+// const res = add(2,3)//arguments
+// console.log("result ", res);
+
+function msg(user) {
+
+    return `${user} just log`
+    
+}
+
+console.log(msg("ganesh"))
---
+++ demo1/scopes.js
@@ -0,0 +1,8 @@
+if(true){
+    //let a = 20;
+var b = 20;
+//const c = 20;
+}
+//console.log(a);
+console.log(b);
+//console.log(c);
