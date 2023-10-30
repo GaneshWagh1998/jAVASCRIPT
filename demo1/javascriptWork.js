ba1e08114dbfc7eba3cdf992aa8286bc940637d7
GaneshWagh1998 <63993358+GaneshWagh1998@users.noreply.github.com>
2023-10-30 07:16:33 +0000

    execution

--- demo1/javascriptWork.js
+++ demo1/javascriptWork.js
@@ -1,6 +1,31 @@
 /*
 **********Javascript Execution Context***********
 
+1) {} -> Global Execution Context 
+         ((this)Global value is window object in browser)
+2) Function Execution Context
+3) Eval Execution Context
+
+Phase For Javascript Execution :-
+
+- Memory Creation/ Allocation Phase
+- Execution Proces
+  1. Global Execution Context starts
+     this windows object executed
+  {2. Memory Creation Phase
+    - variable memory alocated
+  3. Execution phase
+    - values allocated to variable
+    - If function is called then new execution cycle start for that function 
+      1. Memory Phase
+       - varaible memory allocated
+      2. Execution phase
+       - varibel allocated & operation done 
+      3. then all avlues return to global execution context (phase 1 )}
+
+      call Stack 
+       
+      
 
 
 
