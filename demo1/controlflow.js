7a7249252a823382a686110243fbb00e825f73d8
GaneshWagh1998 <63993358+GaneshWagh1998@users.noreply.github.com>
2023-10-31 10:07:31 +0000

    map

---
+++ demo1/controlflow.js
@@ -0,0 +1,16 @@
+//  Truthy values & falsy value
+//False values
+// false ,0,-0,BugInt , null, "", undefined , NaN
+//Truthy value :
+//True, "0", [],"false"," ",function(){0}
+//
+//Nullish coalescing operator(??)
+
+val = null ?? 10 ;
+
+//Ternary Operator
+
+//condition ? true : false
+
+const tt= 100;
+tt >= 80 ? console.log("true"): console.log("false");; 
---
+++ demo1/iteration.js
@@ -0,0 +1,46 @@
+// for (let a = 0; a < 10; a++) {
+//     const element = a;
+//     console.log(element);
+// }
+
+//break and continue
+// const arr = "hello"
+//  for (const i of arr) {
+//     console.log(i);
+//  }
+//Maps
+
+// const map = new Map();
+// map.set('india',"maharshtra")
+// map.set('usa', 'los angelos');
+
+//console.log(map);
+
+// for (const [key,value] of map) {
+//     console.log(key," ",value);
+    
+// }
+
+
+//for in loop
+
+// for (const key in object) {
+    
+        
+//     }
+
+//for each
+
+const array= ['abc','abn','abcx','adfgx'];
+// 
+// array.forEach(element => {
+//     console.log(element);
+    
+// });
+
+const newnum = array.filter((item)=>{
+  return item
+})
+
+
+console.log(newnum);
